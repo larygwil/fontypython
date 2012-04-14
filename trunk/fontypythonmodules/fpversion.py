fdd22741b71acdf957d09b2189c1599750b8c5ef
donningle <donningle@10e3e7b3-dc37-4a6a-98fe-1e456c7b9c7b>
2012-04-14 14:02:28 +0000

    Changed the version string in trunk to "TRUNK". I am going to edit it to a specific version in the releases/ directory, when I make a release.
    Hope this does not backfire.

--- trunk/fontypythonmodules/fpversion.py
+++ trunk/fontypythonmodules/fpversion.py
@@ -15,4 +15,4 @@
 ##	You should have received a copy of the GNU General Public License
 ##	along with Fonty Python.  If not, see <http://www.gnu.org/licenses/>.
 
-version = "0.4.3"
+version = "TRUNK" #0.4.3 <-- I am going to alter this in releases/ dir only. After a branch has been made etc.
