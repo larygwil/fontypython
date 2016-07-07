0496efa75984d67b2226ef245927a45ec5fa361a
donningle <donningle@10e3e7b3-dc37-4a6a-98fe-1e456c7b9c7b>
2016-07-07 13:01:53 +0000

    Altering help screen's width.

--- trunk/fontypythonmodules/wxgui.py
+++ trunk/fontypythonmodules/wxgui.py
@@ -324,7 +324,8 @@ class MainFrame(wx.Frame):
 		# =========
 		# Made the initial size of the help dialog smaller
 		# Was requested by a user who has a small screen
-		dlg = dialogues.DialogHelp(self, size=(400, 400))
+		# 600px wide is about as narrow as I can get it...
+		dlg = dialogues.DialogHelp(self, size=(600, 400))
 		val = dlg.ShowModal()
 		dlg.Destroy()
 
