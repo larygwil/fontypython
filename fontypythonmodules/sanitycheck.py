3de0d9c4090d860fb3ae309219c676d32c545c22
donningle <donningle@10e3e7b3-dc37-4a6a-98fe-1e456c7b9c7b>
2017-12-05 09:13:29 +0000

    Subtle alteration to startup process.

--- fontypythonmodules/sanitycheck.py
+++ fontypythonmodules/sanitycheck.py
@@ -14,6 +14,10 @@
 ##
 ## You should have received a copy of the GNU General Public License
 ## along with Fonty Python.  If not, see <http://www.gnu.org/licenses/>.
+"""
+This is step 5 of the startup process. We got here from
+    start.py
+"""
 
 import sys
 import strings
